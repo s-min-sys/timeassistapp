ce5c6c865cd7397adb62c3dda10ce7ff1b34c03e
patdz <patdz@ymipro.com>
2023-02-24 09:45:24 +0800

    u

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -6,11 +6,8 @@ class MainWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('时间助手'),
-      ),
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: TasksWidget(),
       ),
     );
--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -4,39 +4,23 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:confirm_dialog/confirm_dialog.dart';
 
 import 'global.dart';
-
-Future<List<Task>> fetchTasks() async {
-  print('XXX ${Global.token}');
-  final response = await http.get(
-    Uri.parse('${dotenv.env['SERVER_DOMAIN']}/tasks'),
-    headers: {
-      'Authorization': 'Basic ${Global.token}',
-    },
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    List responseJson = json.decode(response.body);
-    return responseJson.map((m) => Task.fromJson(m)).toList();
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    return List<Task>.empty();
-  }
-}
 
 class Task {
   final String id;
   final String title;
   final String subTitle;
+  final bool alarmFlag;
+  final int taskType;
 
   const Task({
     required this.id,
     required this.title,
     required this.subTitle,
+    required this.alarmFlag,
+    required this.taskType,
   });
 
   factory Task.fromJson(Map<String, dynamic> json) {
@@ -44,10 +28,23 @@ class Task {
     if (json.containsKey('sub_title')) {
       subTitle = json['sub_title'];
     }
+
+    var alarmFlag = false;
+    if (json.containsKey('alarm_flag')) {
+      alarmFlag = json['alarm_flag'];
+    }
+
+    var taskType = 0;
+    if (json.containsKey('vo_task_type')) {
+      taskType = json['vo_task_type'];
+    }
+
     return Task(
       id: json['id'],
       title: json['value'],
       subTitle: subTitle,
+      alarmFlag: alarmFlag,
+      taskType: taskType,
     );
   }
 }
@@ -62,6 +59,35 @@ class TasksWidget extends StatefulWidget {
 class _TaskWidgetState extends State<TasksWidget> {
   late Future<List<Task>> futureAlbum;
   Timer? _timer;
+  int refreshCounter = 12;
+  DateTime dateTime = DateTime.now();
+
+  Future<List<Task>> fetchTasks() async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['SERVER_DOMAIN']}/tasks'),
+      headers: {
+        'Authorization': 'Basic ${Global.token}',
+      },
+    );
+
+    setState(() {
+      refreshCounter = 10;
+    });
+
+    if (response.statusCode == 200) {
+      if (mounted) {
+        setState(() {
+          dateTime = DateTime.now();
+        });
+      }
+      List responseJson = json.decode(response.body);
+      return responseJson.map((m) => Task.fromJson(m)).toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      return List<Task>.empty();
+    }
+  }
 
   @override
   void initState() {
@@ -87,10 +113,15 @@ class _TaskWidgetState extends State<TasksWidget> {
     stopRefrsh();
 
     _timer = Timer.periodic(
-      const Duration(seconds: 2),
+      const Duration(seconds: 1),
       (Timer t) => setState(
         () {
-          futureAlbum = fetchTasks();
+          refreshCounter--;
+          if (refreshCounter <= 0) {
+            refreshCounter = 12;
+
+            futureAlbum = fetchTasks();
+          }
         },
       ),
     );
@@ -103,66 +134,152 @@ class _TaskWidgetState extends State<TasksWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: FutureBuilder<List<Task>>(
-                future: futureAlbum,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    startRefreshIfNoTimer();
-
-                    return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) => ListTile(
-                        trailing: IconButton(
-                          icon: const Icon(
-                            Icons.done,
-                            color: Colors.green,
-                          ),
-                          onPressed: () {
-                            http.post(
-                              Uri.parse(
-                                  '${dotenv.env['SERVER_DOMAIN']}/tasks/${snapshot.data![index].id}/done'),
-                              headers: {
-                                'Authorization': 'Basic ${Global.token}',
-                              },
-                            ).then((value) => futureAlbum = fetchTasks());
-                          },
-                          tooltip: 'done',
-                        ),
-                        title: Text(
-                          snapshot.data![index].title,
-                        ),
-                        subtitle: Text(
-                          snapshot.data![index].subTitle,
-                        ),
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    stopRefrsh();
-
-                    return IconButton(
-                      icon: const Icon(Icons.network_check),
-                      onPressed: () {
-                        setState(() {
-                          futureAlbum = fetchTasks();
-                        });
-                      },
-                    );
-                  }
-
-                  // By default, show a loading spinner.
-                  return const CircularProgressIndicator();
-                },
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Text('时间助手'),
+            const SizedBox(
+              width: 4,
+            ),
+            Text(
+              dateTime.toString().substring(0, 19),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.black,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            LinearProgressIndicator(
+              value: refreshCounter / 12.0,
+              color: Colors.red,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: RefreshIndicator(
+                  onRefresh: _onRefresh,
+                  child: FutureBuilder<List<Task>>(
+                    future: futureAlbum,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        startRefreshIfNoTimer();
+
+                        return ListView.separated(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) => ListTile(
+                            iconColor: Colors.blue,
+                            textColor: snapshot.data![index].alarmFlag
+                                ? Colors.red
+                                : Colors.black,
+                            trailing: IconButton(
+                              icon: const Icon(
+                                Icons.reply,
+                                color: Colors.green,
+                              ),
+                              onPressed: () async {
+                                if (await confirm(
+                                  context,
+                                  title: const Text('确认注意到了?'),
+                                  content: Text(snapshot.data![index].title),
+                                  textOK: const Text('我知道了'),
+                                  textCancel: const Text('我再想想'),
+                                )) {
+                                  http.post(
+                                    Uri.parse(
+                                        '${dotenv.env['SERVER_DOMAIN']}/tasks/${snapshot.data?[index].id}/done'),
+                                    headers: {
+                                      'Authorization': 'Basic ${Global.token}',
+                                    },
+                                  ).then((value) => futureAlbum = fetchTasks());
+                                } else if (mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Cancelled'),
+                                        duration: Duration(seconds: 1)),
+                                  );
+                                }
+                              },
+                              tooltip: 'done',
+                            ),
+                            title: Row(
+                              children: [
+                                Visibility(
+                                  visible: snapshot.data![index].taskType == 1,
+                                  child: const Icon(
+                                    Icons.task_alt,
+                                  ),
+                                ),
+                                Visibility(
+                                  visible: snapshot.data![index].taskType == 2,
+                                  child: const Icon(
+                                    Icons.alarm,
+                                  ),
+                                ),
+                                const SizedBox(width: 6),
+                                Expanded(
+                                  child: Text(
+                                    snapshot.data![index].title,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            subtitle: Row(
+                              children: [
+                                const SizedBox(width: 30),
+                                Text(
+                                  snapshot.data![index].subTitle,
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const Divider(
+                              color: Colors.grey,
+                              height: 1,
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        stopRefrsh();
+
+                        return IconButton(
+                          icon: const Icon(Icons.network_check),
+                          onPressed: () {
+                            setState(() {
+                              futureAlbum = fetchTasks();
+                            });
+                          },
+                        );
+                      }
+
+                      // By default, show a loading spinner.
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _onRefresh() async {
+    return Future.delayed(const Duration(microseconds: 100), () {
+      setState(() {
+        futureAlbum = fetchTasks();
+      });
+    });
   }
 }
